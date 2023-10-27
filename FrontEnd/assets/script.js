2f261760b018e60dc03981fae33322c306362e07
Pauline Hesse <paulinehesse@macbook-pro-de-pauline.home>
2023-10-27 11:32:24 +0200

    modif categories

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -14,61 +14,60 @@ function showWorks (apiWorks) {
   }
 }
 
+function allCategory (apiWorks) {
+  for (let i = 0; i < apiWorks.length; i++){
+    if (apiWorks[i].categoryId !== 1) {
+      projetDiv.remove()
+    }
+  }
+}
+
+function objetsCategory (apiWorks) {
+  for (let i = 0; i < apiWorks.length; i++){
+    if (apiWorks[i].categoryId !== 1) {
+      projetDiv.remove()
+    }
+  }
+}
+
+function appartementsCategory (apiWorks) {
+  for (let i = 0; i < apiWorks.length; i++){
+    if (apiWorks[i].categoryId !== 2) {
+      projetDiv.remove()
+    }
+  }
+}
+
+function hotelsCategory (apiWorks) {
+  for (let i = 0; i < apiWorks.length; i++){
+    if (apiWorks[i].categoryId !== 3) {
+      projetDiv.remove()
+    }
+  }
+}
+
 function showCategories (apiCategories) {
   const portfolio = document.querySelector(".portfolio")
   for (let i = 0; i < apiCategories.length; i++) {
-    const catButtons = document.createElement("button")
-    const catAll = document.getElementById("tous")
-    const catObjets = document.getElementById("objets")
-    const catAppartements = document.getElementById("appartements")
-    const catHotelEtResto = document.getElementById("hotels")
-    
-    catObjets.innerText = apiCategories[i].categoryId
-    catAppartements.innerText = apiCategories[i].categoryId
-    catHotelEtResto.innerText = apiCategories[i].categoryId
+    const boutonTous = document.getElementById("tous")
+    const boutonObjets = document.getElementById("objets")
+    const boutonAppartements = document.getElementById("appartements")
+    const boutonHotelEtResto = document.getElementById("hotels")
 
-    catButtons.appendChild(catAll)
-    catButtons.appendChild(catObjets)
-    catButtons.appendChild(catAppartements)
-    catButtons.appendChild(catHotelEtResto)
+    boutonObjets.innerText = apiCategories[0].name
+    boutonAppartements.innerText = apiCategories[1].name
+    boutonHotelEtResto.innerText = apiCategories[2].name
 
-    portfolio.appendChild(catButtons)
-
-    console.log(catObjets)
-
-    
+    console.log(boutonObjets)
   }
-
-  // const portfolio = document.querySelector(".portfolio")
-  // for (let i = 0; i < apiCategories.length; i++) {
-  //   const catButtons = document.createElement("button");
-  //   const catAll = new Set("Objets", "1", "Appartements", "2", "Hôtel et restaurants", "3");
-  //   const catObjets = new Set("Objets", "1");
-  //   const catAppartements = new Set("Appartements", "2");
-  //   const catHotelEtResto = new Set("Hôtel et restaurants", "3");
-
-  //   catObjets.innerText = apiCategories[i].category
-  //   catAppartements.innerText = apiCategories[i].category
-  //   catHotelEtResto.innerText = apiCategories[i].category
-
-  //   catButtons.appendChild(catAll)
-  //   catButtons.appendChild(catObjets)
-  //   catButtons.appendChild(catAppartements)
-  //   catButtons.appendChild(catHotelEtResto)
-
-  //   portfolio.appendChild(catButtons)
-
-  //   console.log(catObjets)
-    
-  // }
 }
 
 const init = async()=>{
   const works = await getWorks()
   showWorks(works)
+
   const categories = await getCategories()
   showCategories(categories)
-
 
   console.log(works)
   console.log(categories)
