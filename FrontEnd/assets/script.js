2dd49798a5ef0b0b3428e9a45806b6b997a62aa7
Pauline Hesse <paulinehesse@macbook-pro-de-pauline.home>
2023-10-30 16:23:03 +0100

    modif categories

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,6 @@
 function showWorks (apiWorks) {
   const gallery = document.querySelector(".gallery")
+  gallery.innerHTML = "";
   for (let i = 0; i < apiWorks.length; i++) {
     const projetDiv = document.createElement("figure")
     const projetImg = document.createElement("img")
@@ -15,49 +16,54 @@ function showWorks (apiWorks) {
 }
 
 function showCategories (apiCategories) {
-  // const portfolio = document.querySelector("#portfolio")
-  const boutonTous = document.getElementById("tous")
-  const boutonObjets = document.getElementById("objets")
-  const boutonAppartements = document.getElementById("appartements")
-  const boutonHotelEtResto = document.getElementById("hotelsetresto")
+  const categories = document.querySelector(".categories")
+  const boutonTous = document.createElement("button")
+  const boutonObjets = document.createElement("button")
+  const boutonAppartements = document.createElement("button")
+  const boutonHotelEtResto = document.createElement("button")
 
+  boutonTous.innerText = "Tous"
   boutonObjets.innerText = apiCategories[0].name
   boutonAppartements.innerText = apiCategories[1].name
   boutonHotelEtResto.innerText = apiCategories[2].name
 
-  // portfolio.appendChild(boutonObjets)
-  // portfolio.appendChild(boutonAppartements)
-  // portfolio.appendChild(boutonHotelEtResto)
-  // portfolio.appendChild(boutonTous)
+  categories.appendChild(boutonTous)
+  categories.appendChild(boutonObjets)
+  categories.appendChild(boutonAppartements)
+  categories.appendChild(boutonHotelEtResto)
 
-  console.log(boutonObjets)
+  boutonTous.addEventListener ("click", async function(){
+    const works = await getWorks()
+    const filterWorks = works.filter((work) => work)
+    console.log(filterWorks)
+    showWorks(filterWorks) 
+    console.log("Bouton Tous")
+  })
+  
+  boutonObjets.addEventListener ("click", async function(){
+    const works = await getWorks()
+    const filterWorksObjets = works.filter((work) => work.categoryId == 1)
+    console.log(filterWorksObjets)
+    showWorks(filterWorksObjets) 
+    console.log("Bouton Objets")
+  })
+  
+  boutonAppartements.addEventListener ("click", async function(){
+    const works = await getWorks()
+    const filterWorksAppartement = works.filter((work) => work.categoryId == 2)
+    console.log(filterWorksAppartement)
+    showWorks(filterWorksAppartement) 
+    console.log("Bouton Appartements")
+  })
+  
+  boutonHotelEtResto.addEventListener ("click", async function(){
+    const works = await getWorks()
+    const filterWorksHotel = works.filter((work) => work.categoryId == 3)
+    console.log(filterWorksHotel)
+    showWorks(filterWorksHotel) 
+    console.log("Bouton Hôtels & restaurants")
+  })
 }
-
-
-// function filterButtons (apiWorks, apiCategories){
-//   boutonTous.addEventListener ("click", function(apiWorks){
-//     if (boutonObjets)
-//     console.log("Bouton Tous")
-//     allCategory(apiWorks)
-//   })
-  
-//   boutonObjets.addEventListener ("click", function(apiWorks){
-//     console.log("Bouton Objets")
-//     objetsCategory(apiWorks)
-//   })
-  
-//   boutonAppartements.addEventListener ("click", function(apiWorks){
-//     console.log("Bouton Appartements")
-//     appartementsCategory(apiWorks)
-//   })
-  
-//   boutonHotelEtResto.addEventListener ("click", function(apiWorks){
-//     console.log("Bouton Hôtels & restaurants")
-//     hotelsCategory(apiWorks)
-//   })
-
-//   console.log(filterButtons)
-// }
 
 const init = async()=>{
   const works = await getWorks()
