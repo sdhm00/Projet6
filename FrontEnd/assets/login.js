da765bb490b0820ea79957f714d243b6a0c5af36
Pauline Hesse <paulinehesse@macbook-pro-de-pauline.home>
2023-11-07 11:46:37 +0100

    modif login 7 11 23

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,14 +1,27 @@
 function installConnexion(){
   const form = document.getElementById("form");
+  const errorMessage = document.getElementById("errorMessage");
 
   form.addEventListener("submit",async function(event) {
-      event.preventDefault()
+      event.preventDefault();
       const emailInput = document.getElementById("email").value;
       const passwordInput = document.getElementById("password").value;
 
-      const login = await getLogin(emailInput, passwordInput)
-      console.log(login.token)
+      const login = await getLogin(emailInput, passwordInput);
+
+      
+      const getToken = await JSON.stringify(login.token);
+      window.localStorage.setItem("token", getToken);
+      const token = localStorage.getItem("token")
+
+      if (getToken === null) {
+        errorMessage.style.display = flex;
+      }
+      else {
+        window.location.href = "./index.html";
+      }
   });
+
 };
 
 const init = ()=>{
