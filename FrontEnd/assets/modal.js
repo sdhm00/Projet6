1fe4a44178aa866fdbcf69a9d06152dd19ba552a
Pauline Hesse <paulinehesse@macbook-pro-de-pauline.home>
2023-11-15 11:48:00 +0100

    modif login 15 11 23

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,14 +1,37 @@
-function modalDisplay (event){
-    event.preventDeffault()
+function modalDisplay (){
     const modal = document.getElementById("modal")
     const loginButton = document.getElementById("loginButton")
+    const logoutButton = document.getElementById("logoutButton")
+    const token = localStorage.getItem("token")
 
-    if (token !== null) {
-        loginButton.textContent = "logout";
+    if (localStorage !== null && token !== null) {
+        loginButton.style.display = "none"
+        logoutButton.style.display = "unset"
         modal.style.display = "flex";
     }
     else {
-        loginButton.textContent = "login";
         modal.style.display = "none";
     }
+
+    logoutButton.addEventListener("click", function(){
+        localStorage.removeItem("token");
+        modalDisplay();
+        window.location.reload();
+    })
 }
+
+function addWork(){
+    const buttonModal = document.getElementsById("buttonmodal")
+    const galleryPhoto = document.getElementById("galleryPhoto")
+    const modalGallery = document.getElementsByClassName("modalGallery")
+
+    buttonModal.addEventListener("click",function(){
+        galleryPhoto.style.display="flex";
+    })
+}
+
+function removeWorks(){}
+
+modalDisplay()
+// addWork()
+// removeWorks()
